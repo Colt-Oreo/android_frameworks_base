e7d73b8c566c979cb7e43f41f0758603fd74af5f
ezio84 <brabus84@gmail.com>
2017-08-28 11:18:41 +0200

    Enable SysUI tuner by default
    Change-Id: I3ecad146b1b7af13824089e5ff099f6acd6954b1

--- packages/SystemUI/src/com/android/systemui/tuner/TunerFragment.java
+++ packages/SystemUI/src/com/android/systemui/tuner/TunerFragment.java
@@ -125,7 +125,7 @@ public class TunerFragment extends PreferenceFragment {
         return super.onOptionsItemSelected(item);
     }
 
-    public static class TunerWarningFragment extends DialogFragment {
+    /*public static class TunerWarningFragment extends DialogFragment {
         @Override
         public Dialog onCreateDialog(Bundle savedInstanceState) {
             return new AlertDialog.Builder(getContext())
@@ -139,5 +139,5 @@ public class TunerFragment extends PreferenceFragment {
                         }
                     }).show();
         }
-    }
+    }*/
 }
