a8331225d0c029da6cb02721857bda2e6b3888d3
Decad3nce <decad3nce@gmail.com>
2017-09-06 00:37:11 +0800

    SystemUI: Add LTE Tile
    works only with 1 sim phone (maybe will rework @xyyx :))

--- telephony/java/android/telephony/TelephonyManager.java
+++ telephony/java/android/telephony/TelephonyManager.java
@@ -5173,6 +5173,17 @@ public class TelephonyManager {
         }
     }
 
+    /**
+     * {@hide}
+     */
+    public void toggleLTE(boolean on) {
+        try {
+            getITelephony().toggleLTE(on);
+        } catch (RemoteException e) {
+            //Silently fail
+        }
+    }
+
     /** @hide */
     @SystemApi
     @RequiresPermission(android.Manifest.permission.CALL_PHONE)
