omnirom/android_frameworks_base_old

base: never crash SystemUI
Change-Id: Iac508b7ef57f7d9ebf084f9570cdd815bf6e2bd6

services/java/com/android/server/SystemServer.java
@@ -250,10 +250,14 @@ public final class SystemServer {
         }
         @Override
         public void onChange(boolean selfChange) {
-            int adbPort = Settings.Secure.getInt(mContentResolver,
-                Settings.Secure.ADB_PORT, 0);
-            // setting this will control whether ADB runs on TCP/IP or USB
-            SystemProperties.set("service.adb.tcp.port", Integer.toString(adbPort));
+            try {
+                int adbPort = Settings.Secure.getInt(mContentResolver,
+                    Settings.Secure.ADB_PORT, 0);
+                // setting this will control whether ADB runs on TCP/IP or USB
+                SystemProperties.set("service.adb.tcp.port", Integer.toString(adbPort));
+            } catch (Exception e) {
+                // never ever crash here
+            }
         }
     }
 

Colt-Oreo/android_frameworks_base

services/java/com/android/server/SystemServer.java
@@ -289,10 +289,15 @@ public final class SystemServer {
         }
         @Override
         public void onChange(boolean selfChange) {
-            int adbPort = LineageSettings.Secure.getInt(mContentResolver,
-                LineageSettings.Secure.ADB_PORT, 0);
-            // setting this will control whether ADB runs on TCP/IP or USB
-            SystemProperties.set("adb.network.port", Integer.toString(adbPort));
+            try {
+                int adbPort = LineageSettings.Secure.getInt(mContentResolver,
+                    LineageSettings.Secure.ADB_PORT, 0);
+                // setting this will control whether ADB runs on TCP/IP or USB
+                SystemProperties.set("adb.network.port", Integer.toString(adbPort));
+            } catch (Exception e) {
+                // never ever crash here
+                Slog.e(TAG, "AdbPortObserver", e);
+            }
         }
     }
 